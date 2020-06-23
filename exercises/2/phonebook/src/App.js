af9138d8bfe52723f5c8c42ad0ba403fee8dbca1
Daniel Vahla <daniel.vahla@siemens.com>
2020-06-23 14:19:02 +0300

    exc 2.6

---
+++ exercises/2/phonebook/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+  const addName = (event) => {
+    event.preventDefault()
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={(event) => setNewName(event.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((val) => <li key={val.name}>{val.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+export default App
