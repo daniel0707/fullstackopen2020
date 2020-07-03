a05be291a981f72e6efe5e63661afd298ac8c246
Daniel Vahla <daniel.vahla@siemens.com>
2020-07-03 13:56:37 +0300

    exc 3.4

--- exercises/3/index.js
+++ exercises/3/index.js
@@ -44,6 +44,17 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    if (persons.some(p => p.id === id)) {
+        persons = persons.filter(p => p.id !== id)
+        res.status(204).end()
+    } else {
+        res.status(404).end()
+    }
+
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
