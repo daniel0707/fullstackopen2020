4f74f5569238a2e1de33973c3a550edeb2a1eeaf
Daniel Vahla <daniel.vahla@siemens.com>
2020-08-17 15:09:46 +0300

    exc 4.15

--- exercises/4/bloglist/app.js
+++ exercises/4/bloglist/app.js
@@ -4,10 +4,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 
 const app = express();
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
 
 logger.info('connecting to', config.MONGODB_URI);
 
@@ -25,6 +28,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
