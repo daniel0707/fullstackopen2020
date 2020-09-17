49966bb03ae5d55f26e9ebc21ab4288fa17860df
Daniel Vahla <daniel.vahla@siemens.com>
2020-09-17 09:41:34 +0300

    exc 5.7

--- exercises/5/bloglist-frontend/src/components/Blog.js
+++ exercises/5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+  if (showDetails) {
+    return (
+      <div style={blogStyle}>
+        {blog.title}
+        <button onClick={toggleDetails}>hide</button>
+        <br/>
+        {blog.url}
+        <br/>
+        {blog.likes}
+        <br/>
+        {blog.author}
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}<button onClick={toggleDetails}>view</button>
+      </div>
+    )
+  }
+}
 
 export default Blog
