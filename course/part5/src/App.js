5f6dacb56cc462c935e2f0afdb682f535c788391
Daniel Vahla <daniel.vahla@siemens.com>
2020-09-27 18:42:31 +0300

    course part5b2

--- course/part5/src/App.js
+++ course/part5/src/App.js
@@ -52,7 +52,7 @@ const App = () => {
       .then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
-      .catch(error => {
+      .catch(() => {
         setErrorMessage(
           `Note '${note.content}' was already removed from server`
         )
@@ -141,4 +141,4 @@ const App = () => {
   )
 }
 
-export default App 
+export default App
--- course/part5/src/components/LoginForm.js
+++ course/part5/src/components/LoginForm.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 
 const LoginForm = ({
   handleSubmit,
@@ -14,14 +16,14 @@ const LoginForm = ({
       <form onSubmit={handleSubmit}>
         <div>
           username
-         <input
+          <input
             value={username}
             onChange={handleUsernameChange}
           />
         </div>
         <div>
           password
-         <input
+          <input
             type="password"
             value={password}
             onChange={handlePasswordChange}
@@ -32,5 +34,11 @@ const LoginForm = ({
     </div>
   )
 }
-
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+}
 export default LoginForm
