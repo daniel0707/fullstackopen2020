ccf4b839b449754dff95fcf40978dee3f58e3a45
Daniel Vahla <daniel.vahla@siemens.com>
2020-10-15 21:11:49 +0300

    exc 6.21

--- exercises/6/redux-anecdotes/src/components/Notification.js
+++ exercises/6/redux-anecdotes/src/components/Notification.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+  const notification = props.notification
   const style = {
     border: 'solid',
     padding: 10,
@@ -18,5 +18,10 @@ const Notification = () => {
     return null
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    notification: state.notification.content,
+  }
+}
 
-export default Notification
+export default connect(mapStateToProps)(Notification)
--- exercises/6/redux-anecdotes/src/reducers/notificationReducer.js
+++ exercises/6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,27 +1,36 @@
-const notificationReducer = (state = '', action) => {
+const notificationReducer = (
+  state = { 'content': '', 'timeoutId': null }, action) => {
   switch (action.type) {
   case 'SET_NOTIFICATION':
-    return action.notification
+    return Object.assign({}, state, { 'content': action.content })
+  case 'SET_TIMEOUT_ID':
+    if (state.timeoutId) clearTimeout(state.timeoutId)
+    return Object.assign({}, state, { 'timeoutId': action.timeoutId })
   default:
     return state
   }
 }
 
-export const createNotification = (notification, timeout) => {
-  return async dispatch => {
+export const createNotification = (content, timeout) => {
+  return dispatch => {
     dispatch({
       type: 'SET_NOTIFICATION',
-      notification: notification
+      content: content
     })
-    await new Promise(resolve => setTimeout(() => {
-      resolve(
-        dispatch({
-          type: 'SET_NOTIFICATION',
-          notification: ''
-        })
-      )
-    }, timeout
-    ))
+    const timeoutId = setTimeout(() => {
+      dispatch({
+        type: 'SET_NOTIFICATION',
+        content: ''
+      })
+      dispatch({
+        type: 'SET_TIMEOUT_ID',
+        timeoutId: null
+      })
+    }, timeout)
+    dispatch({
+      type: 'SET_TIMEOUT_ID',
+      timeoutId: timeoutId
+    })
   }
 }
 
