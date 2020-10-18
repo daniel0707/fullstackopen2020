f2ce5bbdcfa5e20b598f8fbf9c55b0ba6970a3a5
Daniel Vahla <daniel.vahla@siemens.com>
2020-10-18 21:35:11 +0300

    course part 7b

---
+++ course/part7/counter/src/App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+const App = () => {
+  const useCounter = () => {
+    const [value, setValue] = useState(0)
+
+    const increase = () => {
+      setValue(value + 1)
+    }
+
+    const decrease = () => {
+      setValue(value - 1)
+    }
+
+    const zero = () => {
+      setValue(0)
+    }
+
+    return {
+      value,
+      increase,
+      decrease,
+      zero
+    }
+  }
+  const left = useCounter()
+  const right = useCounter()
+
+  return (
+    <div>
+      {left.value}
+      <button onClick={left.increase}>
+        left
+      </button>
+      <button onClick={right.increase}>
+        right
+      </button>
+      {right.value}
+    </div>
+  )
+}
+
+export default App
