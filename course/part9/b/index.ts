b0c71e2a42a14e344d897fabeb5f72cf3a36b2c2
Daniel Vahla <daniel.vahla@siemens.com>
2020-12-07 15:20:37 +0200

    course part9b3

--- course/part9/b/index.ts
+++ course/part9/b/index.ts
@@ -1,8 +1,16 @@
+import { calculator, Operation } from './calculator';
 import express from 'express'
 const app = express();
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
+});
+
+app.get('/calculate', (req, res) => {
+  const { value1, value2, op } = req.query
+
+  const result = calculator(Number(value1), Number(value2), <Operation>op)
+  res.send(result);
 });
 
 const PORT = 3003;
