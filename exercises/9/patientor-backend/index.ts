30dcf9358eee72875cf0c7a9e6055e08eaf62701
Daniel Vahla <daniel.vahla@siemens.com>
2020-12-09 12:34:50 +0200

    exc 9.9

--- exercises/9/patientor-backend/index.ts
+++ exercises/9/patientor-backend/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
+app.use(express.static('static'));
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
